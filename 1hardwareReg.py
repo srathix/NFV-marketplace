c02952950f3d243c8e66fe289bb4d07e173da71b
srathix <44056044+srathix@users.noreply.github.com>
2019-05-27 17:33:18 +0530

    Delete 1hardwareReg.py

--- 1hardwareReg.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-hid = int(input("What's the id of your hardware"))
-hid1 = str(hid)
-subprocess.check_call(["/media/avivek/New Volume/Today/miner1/1_hardwareReg.sh",name,location,hSpec,swSpec,hid1])
